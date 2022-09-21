10a69abe520c2164cf2c98c0912ae3a76adb5d31
Luana Rogiski <luanakaroline688@gmail.com>
2022-09-20 23:08:23 -0300

    No UserController na function register foi feito o request para enviar os dados de um usuário para o servidor

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,27 @@
 
     namespace App\Http\Controllers;
 
+    use App\Models\User;
     use Illuminate\Http\Request;
 
     class UserController extends Controller
     {
 
-        public function register()
+        public function register(Request $request)
         {
+            if ($request->isMethod(Request::METHOD_POST)) {
+                //cadastro user
+                $user = new User();
+                $user->nome = $request->input('nome');
+                $user->sobrenome = $request->input('sobrenome');
+                $user->email = $request->input('email');
+                $user->senha = $request->input('senha');
+                $user->confirmSenha = $request->input('confirmSenha');
+                $user->save();
+
+                echo json_encode(['mensagem' => 'Usuário cadastrado com sucesso']);
+                return;
+            }
             return view('user.register');
         }
 
