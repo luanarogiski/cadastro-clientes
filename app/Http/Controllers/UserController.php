ad5d55736764b787581ecfacc5bdbd394886f88f
Luana Rogiski <luanakaroline688@outlook.com>
2022-11-09 10:01:08 -0300

    Primeiramente foi excluído a migration e model user para testes de acesso do usuario, após isso ambas foram criadas novamente para fazer a validação de acessos. Na view index.blade feito pequena alteração no else Administrador. No Controllers/Controller.php na function checkLogin foi feito a validação de login caso o administrador ou usuário logar.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,31 +15,30 @@
         {
 
             if ($request->isMethod(Request::METHOD_POST)) {
-                $user = User::where('email', $request->input('email'))
-                        ->where('senha', md5($request->input('senha')))
-                        ->first();
-
+                // Primeiro procura o admin
                 $admin = Admin::where('email', $request->input('email'))
                     ->where('senha', md5($request->input('senha')))
                     ->first();
-
-                if ($user) {
-                    $request->session()->put('usuario', $user->id);
-                    echo json_encode(['sucesso' => true, 'mensagem' => 'Usuário logado com sucesso']);
-                    return;
-                } elseif ($request->session()->pull('usuario')) {
-                    //$request->session()->pull('usuario');
-                    echo json_encode(['sucesso' => false, 'mensagem' => 'Usuário ou Senha incorreta']);
-                    return;
-                } elseif ($admin) {
+                if ($admin) { // Se encontrou o admin (senha está correta), então salva na sessão
                     $request->session()->put('admin', $admin->id);
                     echo json_encode(['sucesso' => true, 'mensagem' => 'Admin logado com sucesso']);
                     return;
-                } else {
-                    $request->session()->pull('admin');
-                    echo json_encode(['sucesso' => false, 'mensagem' => 'Admin ou Senha incorreta']);
-                    return;
+                } else { // Se não encontrou o admin, então tenta procurar o usuário
+                    $user = User::where('email', $request->input('email'))
+                        ->where('senha', md5($request->input('senha')))
+                        ->first();
+                    if ($user) { // Se encontrou o usuário (senha está correta), então salva na sessão
+                        $request->session()->put('usuario', $user->id);
+                        echo json_encode(['sucesso' => true, 'mensagem' => 'Usuário logado com sucesso']);
+                        return;
+                    }
                 }
+                // Se não encontrou nem admin e nem usuário, limpa a sessão dos dois
+                // e retorna msg de usuário ou senha incorreta
+                $request->session()->pull('usuario');
+                $request->session()->pull('admin');
+                echo json_encode(['sucesso' => false, 'mensagem' => 'Usuário ou Senha incorreta']);
+                return;
             }
             return view('user.login');
         }
