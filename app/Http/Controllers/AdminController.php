07dd29566c7179b8517eeb473b24abcc0b8f8241
Luana Rogiski <luanakaroline688@outlook.com>
2022-11-10 11:08:29 -0300

    Foram excluídas e novamente criadas a model e migration Admin, para refazer algumas alterações necessárias, e no AdminController foram iniciadas modificações na function register para que primeiramente a empresa que desejar o sistema possa criar seu administrador do sistema.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,29 +10,21 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-            // ADMIN
-    public function index(Request $request)
-    {
-
-    }
-
-
-
 
             // REGISTER
     public function register(Request $request)
     {
         if ($request->isMethod(Request::METHOD_POST)) {
-            //cadastro user
-            $user = new User();
-            $user->nome = $request->input('nome');
-            $user->sobrenome = $request->input('sobrenome');
-            $user->email = $request->input('email');
-            $user->senha = md5($request->input('senha'));
-            $user->confirmSenha = md5($request->input('confirmSenha'));
-            $user->save();
+            //cadastro admin
+            $admin = new Admin();
+            $admin->nome = $request->input('nome');
+            $admin->sobrenome = $request->input('sobrenome');
+            $admin->email = $request->input('email');
+            $admin->senha = md5($request->input('senha'));
+            $admin->confirmSenha = md5($request->input('confirmSenha'));
+            $admin->save();
 
-            echo json_encode(['mensagem' => 'Usuário cadastrado com sucesso']);
+            echo json_encode(['mensagem' => 'Administrador Cadastrado com Sucesso']);
             return;
         }
         return view('admin.register');
--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -131,5 +131,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="{{"/assets/js/scripts.js"}}"></script>
     <script src="{{"/assets/js/admin/register.js"}}"></script>
+    <script src="{{"/assets/js/clientes/index.js"}}"></script>
     </body>
     </html>
